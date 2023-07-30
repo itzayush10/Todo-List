cc3b16cfe575b984f592e7f3a2aa0782ca9f5b20
itzayush10 <103747520+itzayush10@users.noreply.github.com>
2023-07-30 19:25:12 +0530

    added about

---
+++ About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+      This is an About Component
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut iusto exercitationem tempore odio quaerat officia a excepturi natus, quos nostrum laudantium modi molestiae voluptatem sed totam veritatis voluptas nesciunt hic?</p>
+    </div>
+  )
+}
+
+export default About
